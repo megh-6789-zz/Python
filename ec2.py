751e80c5af9ea316615e4e631833bebdd3a6a0e1
megh-6789 <boggarapumeghana@gmail.com>
2019-02-03 22:55:02 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -2,7 +2,7 @@ import boto.ec2
 import time
 Import subprocess
 conn = boto.ec2.connect_to_region("us-west-2")
-t=conn.run_instances(image_id='ami-05cf2265', key_name='newkey',instance_type='t2.micro',placement=’us-west-2a’,subnet_id=’subnet-6c788b25’,security_group_ids=[‘sg-48482d30’]]
+t=conn.run_instances(image_id='ami-05cf2265', key_name='newkey',instance_type='t2.micro',placement=’us-west-2a’,subnet_id=’subnet-****’,security_group_ids=[‘*******’]]
 )
 vol = conn.create_volume(1, "us-west-2a")
 print 'Volume Id: ', vol.id
